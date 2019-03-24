8f6d2439b68eb3bd4b3068e1cd60e56b74733c2b
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-03-24 21:42:49 +0200

    Add files via upload

---
+++ ConsoleApp20/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    public class Car : IFixTotalLost
+    {
+        public string Brand { get; private set; }
+        public bool TotalLost { get; private set; }
+        public bool NeedsRepair { get; set; }
+
+        public Car(string brand, bool totalLost, bool needsRepair)
+        {
+            Brand = brand;
+            TotalLost = totalLost;
+            NeedsRepair = needsRepair;
+            if (TotalLost == true && NeedsRepair == false)
+            {
+                throw new RepairMismatchException($"Shit happens, Man.");
+            }
+
+
+        }
+        public bool CanFixTotalLost()
+        {
+            return false;
+        }
+
+
+
+    }
+}
